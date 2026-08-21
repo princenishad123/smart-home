60b093907dfe416bff2c43d4b4b3be9a249578db
princenishad123 <pn3147399@gmail.com>
2026-08-21 19:21:42 +0530

    url dev chaneg

--- config/socket.ts
+++ config/socket.ts
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
 
-const socket =  io("http://localhost:8080",{
+const url = process.env.NEXT_PUBLIC_NODE_ENV !== "dev" ? process.env.NEXT_PUBLIC_SERVER_URL: "http://localhost:8080"
+
+const socket =  io(url,{
    withCredentials:true
 });
 export default socket
